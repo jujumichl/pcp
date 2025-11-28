c0ceaab6845ac7da3cb8c5fa507e1d93be48b62b
dju <julienmichelin35@gmail.com>
2025-11-28 23:33:28 +0100

    Ajout d'une fonction pour charger un fichier html contenue dans le fichier include, cependant je n'arrive que a charger le footer. Trouver pourquoi le header n'est pas charger avec la fonction (page de test = alt.html

--- loader.js
+++ loader.js
@@ -1,11 +1,36 @@
-const footerHTML = document.getElementById('footer');
-footerHTML.onload(insertFooter);
+// loader.js
+window.addEventListener("load", initApp);
 
-function insertFooter() {
-    fetch('./include/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            footerHTML.innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
+function initApp(){
+    let footer = "includes/footer.html";
+    const footerHTML = document.getElementById('footer');
+    // ne fonctione pas a voir pourquoi !!!!
+    let header = "includes/header.html";
+    const headerHTML = document.getElementById('head-content');
+    insertHTMLFile(footer, footerHTML);
+    insertHTMLFile(header, headerHTML);
 }
+async function insertHTMLFile(htmlFile, htmlElement) {
+  try {
+    const response = await fetch(htmlFile);
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+    }
+
+    const htmlContent = await response.text();
+    
+    // Insérer le contenu
+    if (htmlElement) { // Vérification de l'existence de l'élément
+        htmlElement.innerHTML = htmlContent;
+        console.log("Contenu du fichier .html charger dans " + htmlElement.id);
+    } else {
+        console.error("Élément" + htmlElement.id + " non trouvé.");
+    }
+
+  } catch (error) {
+    console.error("Erreur lors de la récupération du fichier:", error);
+  }
+}
+
+
